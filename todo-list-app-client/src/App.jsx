161b358166ad88e31eef5b2014b9e43eff2d649c
BatuhanYigit <batuhanyigit34@gmail.com>
2024-02-03 18:55:39 +0300

    Add Future
    Add create tasks
    Add tasks count
    Add task status

--- todo-list-app-client/src/App.jsx
+++ todo-list-app-client/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CreateTask from './components/CreateTask'
 import ListTasks from './components/ListTasks'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const [tasks, setTasks] = useState([])
 
+  console.log("Tasks", tasks)
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")))
+  }, [])
+
   return (
-    <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16">
-      <CreateTask tasks={tasks} setTasks={setTasks} />
-      <ListTasks />
-    </div>
+    <>
+      <Toaster />
+      <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16 pt-32">
+        <CreateTask tasks={tasks} setTasks={setTasks} />
+        <ListTasks tasks={tasks} />
+      </div>
+    </>
   )
 }
 
--- todo-list-app-client/src/components/ListTasks.jsx
+++ todo-list-app-client/src/components/ListTasks.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+
 const ListTasks = ({ tasks, setTasks }) => {
-    return (<div>List</div>);
+
+    const [todos, setTodos] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [closed, setClosed] = useState([]);
+
+    useEffect(() => {
+        const fTodos = tasks.filter((task) => task.status === "todo");
+        const fInProgress = tasks.filter((task) => task.status === "inprogress");
+        const fClosed = tasks.filter((task) => tasks.status === "closed");
+
+        setTodos(fTodos)
+        setInProgress(fInProgress)
+        setClosed(fClosed)
+
+    }, [tasks]);
+
+    const statuses = ["todo", "inprogress", "closed"]
+    return (<div className="flex gap-16">
+        {statuses.map((status, index) => <Section
+            key={index}
+            status={status}
+            tasks={tasks}
+            setTasks={setTasks}
+            todos={todos}
+            inProgress={inProgress}
+            closed={closed} />)}
+    </div>);
 }
 
 export default ListTasks;
+
+const Section = ({ status, tasks, setTasks, todos, inProgress, closed, }) => {
+    let text = "Todo";
+    let bg = "bg-slate-500";
+    return (<div className="w-64">
+        <Header text={text} bg={bg} count={todos.length} /> List
+    </div>);
+}
+
+const Header = ({ text, bg, count }) => {
+    return (<div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`} >
+
+        {text}{" "}
+        <div className="nl-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
+            {count}</div>
+    </div>);
+}
