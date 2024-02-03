23194240ceb3a863c0b25e56aa0fab9b3446b72e
BatuhanYigit <batuhanyigit34@gmail.com>
2024-02-03 16:44:03 +0300

    Add create components
    Add create components

---
+++ todo-list-app-client/src/components/CreateTask.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const CreateTask = ({ tasks, setTasks }) => {
+    const [tasks, setTasks] = useState({
+        id: "",
+        name: "",
+        status: "todo",
+    });
+    return (<form>
+        <input type="text"
+            className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"></input>
+        <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">Create</button>
+    </form>);
+}
+
+export default CreateTask;
