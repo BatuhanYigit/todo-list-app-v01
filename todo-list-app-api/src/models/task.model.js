b68bec03a673c6d28bbbec835d92a57ac45672ab
BatuhanYigit <batuhanyigit34@gmail.com>
2024-02-05 15:43:21 +0300

    Add Crud api
    Add Create task
    Add Getall task
    Add Delete task

---
+++ todo-list-app-api/src/models/task.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const taskShema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+},
+    { collection: "tasks", timestamps: true })
+
+const task = mongoose.model("tasks", taskShema)
+
+module.exports = task
