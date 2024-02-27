f76338450148b346e5e291f2676fba2760228608
BatuhanYigit <batuhanyigit34@gmail.com>
2024-02-27 10:33:10 +0300

    Test

--- todo-list-app-api/src/db/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect(process.env.DB_URL)
-    .then(() => {
-        console.log("Db connect successfuly")
-    })
-    .catch((err) => {
-        console.log("Db connect failed : ", err)
-    })
