140fe4758f24f4a5610d92b243df7df00da838bf
BatuhanYigit <batuhanyigit34@gmail.com>
2024-02-27 11:14:48 +0300

    Edit env connection db
    Edit env connection db

--- api/src/db/dbConnection.js
+++ api/src/db/dbConnection.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose")
 
 mongoose.connect(process.env.DB_URL)
     .then(() => {
+        console.log("Envvv", process.env.DB_URL)
         console.log("Db connect successfuly")
     })
     .catch((err) => {
+        console.log("Envvv", process.env.DB_URL)
         console.log("Db connect failed : ", err)
     })
